1a837057a91807e359101892f271e5d5b759822e
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2020-06-02 11:20:49 -0400

    fix(context): length to max

--- src/context/context.validator.ts
+++ src/context/context.validator.ts
@@ -17,8 +17,8 @@ const updateLayerModel = createLayerModel.keys({
 export class ContextValidator {
   static updateModel = Joi.object().keys({
     scope: Joi.string().valid('public', 'protected', 'private'),
-    uri: Joi.string().length(64),
-    title: Joi.string().length(128),
+    uri: Joi.string().max(64),
+    title: Joi.string().max(128),
     icon: Joi.string().allow(''),
     map: Joi.object().keys({
       view: Joi.object().keys({
